e328ae81debdb086f0b2d3ba8446523e9ceaceb6
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-17 11:04:34 -0300

    Componente Rodapé

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy: 2022
+            <a href="http://cod3r.com.br" target="_blank"> cod3r </a>.
+        </strong>
+    </footer>
+)
