821b5053727b1a65729c42b1c500f8042da0bcf2
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-18 11:09:56 -0300

    Componente Tab Content

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -6,6 +6,8 @@ import Tabs from '../common/table/tabs'
 import TabsHeader from '../common/table/tabsHeader'
 import TabsContent from '../common/table/tabsContent'
 import TabHeader from '../common/table/tabHeader'
+import TabContent from '../common/table/tabContent'
+
 
 class BillingCycle extends Component {
     render (){
@@ -21,6 +23,11 @@ class BillingCycle extends Component {
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>  
                         <TabsContent>
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+
 
                         </TabsContent>
                     </Tabs>
