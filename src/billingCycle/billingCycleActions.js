f4be7b5af75fdb0b200a287938b43c402bbd19ff
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-18 15:06:12 -0300

    Melhorias na Inclusão do Ciclo de Pagamento

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,6 +1,10 @@
 import axios from "axios";
-import { toastr } from "react-redux-toastr";
+import { toastr } from "react-redux-toastr";    
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab} from  '../common/table/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
+
 
 
 //FUNÇÃO QUE DISPARA A ACTION
@@ -13,15 +17,19 @@ export function getList() {
 }   
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
+    return dispatch => {
+          axios.post(`${BASE_URL}/billingCycles`, values)
         .then(resp => {
-            toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+            toastr.success('Sucesso', 'Operação Realizada com sucesso.') //mostrar a msg de SUCESSP 
+            dispatch([resetForm('billingCycleForm'), //DISPARAR QUATRO AÇÕES, RESETAR, PEGAR A LISTA , SELECIONAR A TABLE E MOSTRAR A LISTA
+                getList(),
+                selectTab('tabList'),
+                showTabs('tabList', 'tabCreate')
+            ])
         })
         .catch(e => {
             e.response.data.errors.forEach(error => toastr.error('Erro', error))
         })
-    return {
-        type: 'TEMP'
-
+     }
+        
     }
-}
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM  from "react-dom"
 import { createStore, applyMiddleware } from "redux"
 import { Provider } from 'react-redux'
 import promise from "redux-promise"
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import reducers from './main/reducers'
 import App from "./main/app"
@@ -10,7 +12,7 @@ import App from "./main/app"
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
          <App />
