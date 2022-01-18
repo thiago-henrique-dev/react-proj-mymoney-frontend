62b9c7acdd9484ae0d8541acbc380983274dd179
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-18 15:27:21 -0300

    Field com Componente Personalizado

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import LableAndInput from '../common/form/lableAndInput'
 
 class BillingCycleForm extends Component { 
     render(){
@@ -10,9 +11,12 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name='name' component='input' />
-                    <Field name='month' component='input' />
-                    <Field name='year' component='input' />
+                    <Field name='name' component={LableAndInput} 
+                        label='Nome' cols='12 4' placeholder="Informe o nome"   />
+                    <Field name='month' component={LableAndInput} type="number" 
+                        label="Mês" cols="12 4" placeholder="Informe o mês"/>
+                    <Field name='year' component={LableAndInput} type="number" 
+                        label="Ano" cols="12 4" placeholder="Informe o ano"/>
                 </div>
                 <div className='box-footers'>
                     <button type='submit' className='btn btn-primary'>Submit</button>
---
+++ src/common/form/lableAndInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Grid from '../layout/grid'
+
+export default props => (
+    <Grid cols={props.cols}>
+        <div className="from-group">
+            <label htmlFor={props.name}>{props.label}</label>
+            <input {...props.input } className='form-control'
+                placeholder={props.placeholder}
+                readOnly={props.readOnly}
+                type={props.readOnly} />
+        </div> 
+    </Grid>
+)
